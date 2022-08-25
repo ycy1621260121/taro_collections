d13e0282a380327bb9506632412c8d34ac4b3417
hezhixian <T%J&Sg87ep#AFn>
2022-08-25 11:22:16 +0800

    sixthen commit

--- config/project.js
+++ config/project.js
@@ -1,5 +1,5 @@
 module.exports = {
   projectName: 'testB', //项目为testA，在这里改为testA即可
   testA_appid: 'wxc1bc398b8f605514', //不同项目配置不同的appid，第一次配置完了即可，以前的保留，每次启动改上面的项目名称即可
-  testB_appid: 'wxc1bc398b8f605514'
+  testB_appid: 'wxc1bc398b8f605514' //规范： 项目名称_appid（必须）
 }
